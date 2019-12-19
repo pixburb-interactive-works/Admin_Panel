5870fdbbb3d9eea0d771bbf5befbb0f798e9fa97
Vignesh <vigneshbv02@gmail.com>
2019-12-20 01:31:49 +0530

    19 12 2019 PUSH

--- src/components/app/create-organization/create-organization.component.ts
+++ src/components/app/create-organization/create-organization.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../../services/user.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-create-organization',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrganizationComponent implements OnInit {
 
-  constructor() { }
+  createAdminUser: FormGroup;
+  OTPVerification: FormGroup;
+  errorMessage = '';
+  errorOTPMessage = '';
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.createAdminUser = this.formBuilder.group({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [Validators.required, Validators.minLength(10)]),
+      password: new FormControl(null, Validators.required),
+      retypePassword: new FormControl(null, Validators.required)
+    });
+    this.OTPVerification = this.formBuilder.group({
+      otpField: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+    // $('button[aria-controls="collapseTwo"]').attr('disabled', false).trigger('click').attr('disabled', true);
   }
 
+  createAdmin() {
+    // tslint:disable-next-line:triple-equals
+    if (this.createAdminUser.value.password == this.createAdminUser.value.retypePassword) {
+      this.errorMessage = '';
+      this.userService.addUser(
+        this.createAdminUser.value.firstName,
+        this.createAdminUser.value.lastName,
+        this.createAdminUser.value.phone,
+        this.createAdminUser.value.email,
+        this.createAdminUser.value.password
+      ).subscribe(
+        (data: any) => {
+          if (data.status === 200) {
+            $('button[aria-controls="collapseTwo"]').attr('disabled', false).trigger('click').attr('disabled', true);
+          } else {
+            this.errorMessage = data.displayMessage;
+          }
+        });
+    } else {
+      this.createAdminUser.controls.retypePassword.setErrors({incorrect: true});
+      this.errorMessage = 'Password doesn\'t match';
+    }
+  }
+
+  OTPVerify() {
+    this.userService.otpVerification(
+      this.createAdminUser.value.email,
+      this.OTPVerification.value.otpField
+    ).subscribe(
+      (data: any) => {
+        console.log(data);
+        if (data.status === 200) {
+          $('button[aria-controls="collapseThree"]').attr('disabled', false).trigger('click').attr('disabled', true);
+        } else {
+          this.errorOTPMessage = data.displayMessage;
+        }
+      }
+    );
+  }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {last} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  // private ModelBaseURL = '/users';
-  private ModelBaseURL = '';
+  private ModelBaseURL = '/users';
+  // private ModelBaseURL = '';
 
   constructor(private httpClient: HttpClient) { }
 
   public login(username: string, password: string) {
     return this.httpClient.post('http://localhost:8080' + this.ModelBaseURL + '/login', {email: username, password});
+  }
+
+  public addUser(firstName: string, lastName: string, phone: string, email: string, password: string) {
+    return this.httpClient.post('http://localhost:8080' + this.ModelBaseURL + '/create', { firstName, lastName, phone, email, password});
+  }
+
+  public otpVerification(email: string, otp: string) {
+    return this.httpClient.post( 'http://localhost:8080' + this.ModelBaseURL + '/verify', {email, otp});
   }
 }
